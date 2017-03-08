64465b22a0acad7d529bbe16f86d49cc276bf47c
Ruthotto, Lars <lruthotto@emory.edu>
2017-03-08 11:09:39 -0500

    added more tests for sparse matrices. fail currently. Probably an indexing problem?

--- test/sparse.jl
+++ test/sparse.jl
@@ -18,13 +18,28 @@ Ie2 = full(Aid)
 e = sparse(eye(10))
 a1 = AFArray(e)
 @test issparse(a1)
-
 e2 = SparseMatrixCSC(a1)
 @test e2 == e
 
 I = AFArray(vec(collect(1:n)))
 J = AFArray(vec(collect(1:n)))
-V = AFArray(ones(n))
+V = AFArray(randn(n))
 Aid2 = create_sparse_array(n, n, V, I, J, AF_STORAGE_CSR)
-
 @test issparse(Aid2)
+
+
+Ah = sprandn(2*n,n,.1)
+AH = sparse(AFArray(full(Ah)))
+@test nnz(Ah)==sparse_get_nnz(AH)
+@test norm(Ah.nzval - Array(sparse_get_values(AH)),Inf) < 1e-7
+@test all(Ah .== SparseMatrixCSC(AH))
+(V,R,C,t) = sparse_get_info(AH)
+@test norm(Array(V) - Ah.nzval,Inf) < 1e-7
+@test all(R.==Ah.rowval)
+@test all(C.==Ah.colptr)
+xh = randn(n)
+xd = AFArray(xh)
+t1 = Ah*xh
+t2 = AH*xd
+@test norm(t1-Array(t2),Inf) < 1e-7
+
